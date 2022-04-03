9f6a999157af510a2343404b616a3fc777e95068
KageKowalski <KageKowalski@gmail.com>
2022-04-02 23:45:36 -0700

    Began work on convert_mp4_to_ascii function. This function should now convert all frames of an mp4 to jpg and convert all jpg images to ascii art txt files.

--- mp4_ascii_converter.py
+++ mp4_ascii_converter.py
@@ -3,14 +3,22 @@
 
 # Imports
 import cv2
+from pywhatkit import image_to_ascii_art
 
 
-def convert_mp4_to_ascii(mp4_path):
-    pass
+# Generates a new mp4 from mp4_file_in (String path to input mp4 file) where every frame is replaced with ascii art
+# image_dir (String) is directory where temporary images should be stored
+# mp4_file_out (String) is path + name of oupput mp4 file
+def convert_mp4_to_ascii(mp4_file_in, image_dir, mp4_file_out):
+    image_count = convert_mp4_to_frames(mp4_file_in, image_dir)
+
+    for i in range(image_count):
+        image_to_ascii_art("{}frame{}.jpg".format(image_dir, i), "{}ascii_frame{}.txt".format(image_dir, i))
 
 
 # Converts the frames of the mp4 with path mp4_file_in (String) to jpg images
 # The jpg images are saved to the directory image_dir_out (String)
+# Returns number of images created (Int)
 def convert_mp4_to_frames(mp4_file_in, image_dir_out):
     video_capture = cv2.VideoCapture(mp4_file_in)
     success, image = video_capture.read()
@@ -19,10 +27,7 @@ def convert_mp4_to_frames(mp4_file_in, image_dir_out):
         cv2.imwrite("{}frame{}.jpg".format(image_dir_out, count), image)
         success, image = video_capture.read()
         count = count + 1
-
-
-def convert_frame_to_ascii(image_file):
-    pass
+    return count
 
 
 def convert_ascii_to_mp4(image_files):
